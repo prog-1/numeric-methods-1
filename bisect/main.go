12424f61eb5413ac6e177bf58457bd7a8f37378b
Vladimir Satinov <vovaumni@gmail.com>
2023-10-09 18:16:35 +0300

    .

--- bisect/main.go
+++ bisect/main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println(SolveBisect(1, 5, 0.003, func(x float64) float64 { return x*x*x - 2*x - 5 }))
+	// fmt.Println(SolveBisect(1, 5, 0.001, func(x float64) float64 { return x*x*x - 2*x - 5 }))
+	SolveBisect(1, 5, 1e-3, func(x float64) float64 { return x*x*x - 2*x - 5 })
+	SolveBisect(1, 5, 1e-3, func(x float64) float64 { return 3*x*x*x*x - 4*x*x*x - 12*x*x - 5 })
+	SolveBisect(-1, -5, 1e-3, func(x float64) float64 { return 3*x*x*x*x - 4*x*x*x - 12*x*x - 5 })
+	SolveBisect(1.5, 4.5, 1e-3, func(x float64) float64 { return x - 5*math.Sin(x) - 3.5 })
+	SolveBisect(-1, 1.5, 1e-3, func(x float64) float64 { return (1+x)*math.Pow(2.718, 3*x) - 5 })
+	SolveBisect(0, -3, 1e-3, func(x float64) float64 { return (1+x)*math.Pow(2.718, (-2)*x) - (-8) })
 }
 
 func SolveBisect(a, b float64, eps float64, f func(float64) float64) float64 {
--- newton/main.go
+++ newton/main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println(SolveNewton(5, 0.003, func(x float64) float64 { return x*x*x - 2*x - 5 }))
+	// fmt.Println(SolveNewton(5, 0.001, func(x float64) float64 { return x*x*x - 2*x - 5 }))
+	SolveNewton(5, 0.001, func(x float64) float64 { return x*x*x - 2*x - 5 })
+	SolveNewton(5, 0.001, func(x float64) float64 { return 3*x*x*x*x - 4*x*x*x - 12*x*x - 5 })
+	SolveNewton(-5, 0.001, func(x float64) float64 { return 3*x*x*x*x - 4*x*x*x - 12*x*x - 5 })
+	SolveNewton(4.5, 0.001, func(x float64) float64 { return x - 5*math.Sin(x) - 3.5 })
+	SolveNewton(1.5, 0.001, func(x float64) float64 { return (1+x)*math.Pow(2.718, 3*x) - 5 })
+	SolveNewton(-3, 0.001, func(x float64) float64 { return (1+x)*math.Pow(2.718, (-2)*x) - (-8) })
 }
 
 func SolveNewton(x, eps float64, f func(float64) float64) float64 {
