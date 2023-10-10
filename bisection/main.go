061ce22c9b285bda5f412a3bf414578f56a52e3d
Sp0ozy <imolchanov210@gmail.com>
2023-10-10 23:12:56 +0300

    newton method added

--- bisection/main.go
+++ bisection/main.go
@@ -4,7 +4,7 @@ func SolveBisect(a, b float64, eps float64, f func(float64) float64) float64 {
 	if a > b {
 		a, b = b, a
 	}
-	for b-a > 1e-4 {
+	for b-a > eps {
 		if f((a+b)/2)*f(a) > 0 {
 			a = (a + b) / 2
 		} else {
