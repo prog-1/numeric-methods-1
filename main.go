3ecf84887d3207b3643c9d33af47cf34ce05a773
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-10-15 17:42:51 +0300

    Fixed infinite execution

--- main.go
+++ main.go
@@ -28,19 +28,20 @@ func SolveBisect(a, b float64, eps float64, f func(float64) float64) float64 {
 // SolveSecant uses the secant approximation method to find the root
 // of the given `f` with the given precision `eps`.
 func SolveSecant(a, b float64, eps float64, f func(float64) float64) float64 {
-	// Calculate x2
-	// Check if |f(x2)| <= eps
 	fa, fb := f(a), f(b)
-	c := (fb*a - fa*b) / (fb - fa)
-	for math.Abs(f(c)) > eps {
-		c = (fb*a - fa*b) / (fb - fa)
-		fa, fb = fb, f(c)
+	var c, fc float64
+	for {
+		c = (a*fb - b*fa) / (fb - fa)
+		fc = f(c)
+		if math.Abs(fc) <= eps {
+			return c
+		}
+		a, b, fa, fb = b, c, fb, fc
 	}
-	return c
 }
 
 func main() {
-	fmt.Println(SolveSecant(2, 4, 1e-3, func(x float64) float64 {
-		return x - 5*math.Sin(x) - 3.5
-	})) // 2.094551
+	fmt.Println(SolveSecant(-1.5, 1, 1e-3, func(x float64) float64 {
+		return 2*math.Sin(x) + 1.5
+	})) // -0.84806
 }
--- main_test.go
+++ main_test.go
@@ -37,3 +37,30 @@ func TestSolveBisect(t *testing.T) {
 		}
 	}
 }
+
+func TestSolveSecant(t *testing.T) {
+	const epsilon float64 = 1e-3
+	type Input struct {
+		a, b, eps float64
+		f         func(float64) float64
+	}
+	for _, tc := range []struct {
+		name  string
+		input Input
+		want  float64
+	}{
+		// Test case conditions:
+		// 1. One root within range
+		// 2. No extremums within range
+		{"x-5sin(x)-3.5=0", Input{2, 4, epsilon, func(x float64) float64 {
+			return x - 5*math.Sin(x) - 3.5
+		}}, 3.2013568},
+		{"2sin(x)+1.5", Input{-1.5, 1, epsilon, func(x float64) float64 {
+			return 2*math.Sin(x) + 1.5
+		}}, -0.84806},
+	} {
+		if got := SolveSecant(tc.input.a, tc.input.b, tc.input.eps, tc.input.f); math.Abs(got-tc.want) > tc.input.eps {
+			t.Errorf("SolveSecant failed %s: got = %f, want = %f", tc.name, got, tc.want)
+		}
+	}
+}
