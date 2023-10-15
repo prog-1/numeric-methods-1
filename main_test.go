51a36bb5275264d06a654129ac9d51f4728fe7bd
vay4ie <75880193+vay4ie@users.noreply.github.com>
2023-10-15 15:04:20 +0300

    Made Bisection, Newton's and Secant methods and their comparison

--- main_test.go
+++ main_test.go
@@ -5,33 +5,47 @@ import (
 	"testing"
 )
 
-func TestSolveBisect(t *testing.T) {
+func TestMethods(t *testing.T) {
 	for _, tc := range []struct {
-		name    string
-		formula func(float64) float64
-		a, b, e float64
-		want    float64
+		name     string
+		equation func(float64, *int) float64
+		a, b, e  float64
+		want     float64
+		want2    float64
 	}{
-		{"1", func(x float64) float64 { return (x * x * x) - 2*x - 5 }, -100, 100, 0.005, 2.093505859375},
-		{"2", func(x float64) float64 { return 3*(x*x*x*x) - 4*(x*x*x) - 12*(x*x) - 5 }, -100, 100, 0.005, -1.593017578125},
-		{"3", func(x float64) float64 { return x - 5*math.Sin(x) - 3.5 }, -100, 100, 0.005, 3.2012939453125},
-		{"4", func(x float64) float64 { return (1+x)*math.Pow(10, 3*x) - 5 }, -100, 100, 0.005, 0.2044677734375},
-		{"5", func(x float64) float64 { return (1+x)*math.Pow(10, -2*x) - (-8) }, -100, 100, 0.005, -1.06201171875},
+		{"1", equation1, -1, 3, 0.005, 2.095, 0},
+		{"2", equation2, -5, 5, 0.005, -1.593, 2.824},
+		{"3", equation3, 0, 10, 0.005, 3.201, 8.201},
+		{"4", equation4, -2, 2, 0.005, 0.204, 0},
+		{"5", equation5, -3, 1, 0.005, -1.061, 0},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			got := SolveBisect(tc.formula, tc.a, tc.b, tc.e)
-			if math.Abs(got-tc.want) > tc.e {
-				t.Errorf("got = %v, want = %v with e: %v", got, tc.want, tc.e)
+			t.Log("Equation", tc.name)
+			gotbm, _, _ := bisectionMethod(tc.equation, tc.a, tc.b, tc.e)
+			if math.Abs(gotbm-tc.want) > tc.e && math.Abs(gotbm-tc.want2) > tc.e {
+				t.Errorf("Bisection method: got = %v, want = %v or %v with e: %v", gotbm, tc.want, tc.want2, tc.e)
+			}
+
+			gotnm, _, _ := newtonMethod(tc.equation, tc.a, tc.e)
+			if math.Abs(gotnm-tc.want) > tc.e && math.Abs(gotnm-tc.want2) > tc.e {
+				t.Errorf("Bisection method: got = %v, want = %v or %v with e: %v", gotnm, tc.want, tc.want2, tc.e)
+			}
+
+			gotsm, _, _ := secantMethod(tc.equation, tc.a, tc.b, tc.e)
+			if math.Abs(gotsm-tc.want) > tc.e && math.Abs(gotsm-tc.want2) > tc.e {
+				t.Errorf("Secant method: got = %v, want = %v or %v with e: %v", gotsm, tc.want, tc.want2, tc.e)
 			}
 		})
 	}
 }
 
-func main() {
+// ⬇️ Uncomment for tests ⬇️
+
+func main2() {
 	testing.Main(
 		/* matchString */ func(a, b string) (bool, error) { return a == b, nil },
 		/* tests */ []testing.InternalTest{
-			{Name: "Test Bisection", F: TestSolveBisect},
+			{Name: "Test Methods", F: TestMethods},
 		},
 		/* benchmarks */ nil /* examples */, nil)
 }
